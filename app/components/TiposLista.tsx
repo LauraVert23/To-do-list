a6c7bac9b1c89fb78dc00a1af2a78b2013bf8bef
LauraVert23 <laura.ribeiro@vert-capital.com>
2025-04-03 14:47:49 -0300

    Ajuste responsividade mobile

--- app/components/TiposLista.tsx
+++ app/components/TiposLista.tsx
@@ -19,7 +19,7 @@ function TiposLista() {
 
   return (
     <div className="flex flex-col md:flex-row gap-3 lg:gap-8 justify-center ">
-      <div className="lg:w-[350px] w-[270px] overflow-x-hidden h-[calc(55vh-9rem)]  sm:h-[calc(55vh-6rem)] md:h-[calc(100vh-7rem)]  box-content  bg-cyan-900 shadow-md rounded-lg">
+      <div className="lg:w-[350px] w-[270px] overflow-x-hidden h-[calc(57vh-8rem)]  sm:h-[calc(55vh-5rem)] md:h-[calc(100vh-7rem)]  box-content  bg-cyan-900 shadow-md rounded-lg">
         <div className="lg:mt-3 mt-1 flex justify-between items-center sticky top-0 bg-cyan-900 z-20 w-full px-4 h-8">
           <h1 className="font-extrabold">Pendentes</h1>
           <div
@@ -40,7 +40,7 @@ function TiposLista() {
       </div>
 
       {
-        <div className="lg:w-[350px] w-[270px]  overflow-x-hidden h-[calc(50vh-8rem)] sm:h-[calc(55vh-6rem)] md:h-[calc(100vh-7rem)]  box-content  bg-cyan-900 shadow-md rounded-lg">
+        <div className="lg:w-[350px] w-[270px]  overflow-x-hidden h-[calc(55vh-9rem)] sm:h-[calc(55vh-6rem)] md:h-[calc(100vh-7rem)]  box-content  bg-cyan-900 shadow-md rounded-lg">
           <div className="lg:mt-3 mt-1  flex justify-between items-center sticky top-0 bg-cyan-900 z-20 w-full px-4 h-8">
             <h1 className="font-extrabold">Concluídas</h1>
           </div>
